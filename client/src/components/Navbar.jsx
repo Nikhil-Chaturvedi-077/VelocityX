9b4cef292f13107283152104c659e674e8865821
Nikhil Chaturvedi <chaturvedinikhil077@gmail.com>
2026-08-22 03:29:30 +0530

    Animation Added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,19 +1,43 @@
 import React, { useState } from "react";
 import { assets, menuLinks } from "../assets/assets";
 import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useAppContext } from "../context/AppContext";
+import toast from "react-hot-toast";
+import {motion} from 'motion/react'
 
-const Navbar = ({setShowLogin}) => {
+const Navbar = () => {
+  const {setShowLogin, user, logout, isOwner, axios, setIsOwner} = useAppContext()
+
   const location = useLocation();
   const [open, setOpen] = useState(false);
-  const navigate=useNavigate();
+  const navigate = useNavigate();
+
+  const changeRole = async()=>{
+    try {
+     const {data} = await axios.post('/api/owner/change-role')
+     if(data.success){
+      setIsOwner(true)
+      toast.success(data.message) 
+     }
+     else{
+      toast.error(data.message)
+     }
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
   return (
-    <div
+    <motion.div
+    initial = {{y:-20, opacity: 0}}
+    animate = {{y: 0, opacity:1}}
+    transition = {{duration:0.5}}
       className={`flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32
       py-4 text-gray-600 border-b border-borderColor relative transition-all
       ${location.pathname === "/" && "bg-light"}`}
     >
       <Link to="/">
-        <img src={assets.logo} alt="logo" className="h-8" />
+        <motion.img whileHover={{scale:1.05}} src={assets.logo} alt="logo" className="h-8" />
       </Link>
 
       <div
@@ -35,10 +59,12 @@ const Navbar = ({setShowLogin}) => {
         </div>
 
         <div className='flex max-sm:flex-col items-start sm:items-center gap-6'>
-          <button onClick={()=>navigate('/owner')} className="cursor-pointer" >Dashboard</button>
-          <button onClick={()=>setShowLogin(true)}
+
+          <button onClick={()=>isOwner ? navigate('/owner'):changeRole()} className="cursor-pointer" >{isOwner ? 'Dashboard':'List cars'}</button>
+
+          <button onClick={()=>{user?logout():setShowLogin(true)}}
           className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition-all text-white rounded-lg">
-            Login
+            {user ? 'Logout':'Login'}
           </button>
         </div>
       </div>
@@ -47,7 +73,7 @@ const Navbar = ({setShowLogin}) => {
           <img src={open?assets.close_icon:assets.menu_icon} alt="menu" />
         </button>
 
-    </div>
+    </motion.div>
   );
 };
 
--- client/src/pages/CarDetails.jsx
+++ client/src/pages/CarDetails.jsx
@@ -1,24 +1,45 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
-import { assets, dummyCarData } from '../assets/assets';
+import { assets} from '../assets/assets';
 import Loader from '../components/Loader';
+import { useAppContext } from '../context/AppContext';
+import toast from 'react-hot-toast';
+import { motion } from 'motion/react';
 
 const CarDetails = () => {
 
   const {id} = useParams();
+  const {cars, axios, pickupDate, setPickupDate, returnDate,
+    setReturnDate
+  } = useAppContext();
   const navigate = useNavigate();
   const [car,setCar] = useState(null);
-  const [pickupDate,setPickupDate] = useState("")
   const currency = import.meta.env.VITE_CURRENCY
 
   const handleSubmit = async(e)=>{
     e.preventDefault();
+    try {
+      const {data} =  await axios.post('/api/bookings/create',{
+        car:id,
+        pickupDate,
+        returnDate
+      })
+
+      if(data.success){
+        toast.success(data.meaasge);
+        navigate('/my-bookings')
+      }else{
+        toast.error(data.meaasge);
+      }
+    } catch (error) {
+      toast.error(error.meaasge);
+    }
   }
 
 
   useEffect(()=>{
-    setCar(dummyCarData.find(car => car._id=== id))
-  },[id])
+    setCar(cars.find(car => car._id=== id))
+  },[cars,id])
 
   return car? (
     <div className='px-6 md:px-16 lg:px-24 xl:px-32 mt-16'>
@@ -30,9 +51,22 @@ const CarDetails = () => {
 
         <div className='grid grid-cols-1 lg:grid-cols-3 gap-8 lg:gap-12'>
         {/*left: car image and details */}
-          <div className='lg:col-span-2'>
-            <img src={car.image} alt="" className='w-full h-auto md:max-h-100 object-cover rounded-xl mb-6 shadow-md' />
-            <div className='space-y-6'>
+          <motion.div 
+          initial={{opacity:0, y:30}}
+          animate={{opacity:1, y:0}}
+          transition={{duration:0.6}}
+          className='lg:col-span-2'>
+            <motion.img 
+            initial={{opacity:0, scale:0.98}}
+            animate={{opacity:1, scale:1}}
+            transition={{duration:0.5}}
+
+            src={car.image} alt="" className='w-full h-auto md:max-h-100 object-cover rounded-xl mb-6 shadow-md' />
+            <motion.div className='space-y-6'
+            initial={{opacity:0}}
+            animate={{opacity:1}}
+            transition={{duration:0.5, delay:0.2}}
+            >
               <div>
                 <h1 className='text-3xl font-bold' >{car.brand} {car.model} </h1>
                 <p className='text-gray-500 text-lg'>{car.category} • {car.year}</p>
@@ -54,11 +88,16 @@ const CarDetails = () => {
                       text : car.location
                     }
                   ].map(({icon, text})=>(
-                    <div key={text} className='flex flex-col items-center bg-light
+                    <motion.div 
+                    initial={{opacity:0, y:10}}
+                    animate={{opacity:1, y:0}}
+                    transition={{duration:0.4}}
+
+                    key={text} className='flex flex-col items-center bg-light
                     p-4 rounded-lg' >
                       <img src={icon} alt=""className='h-5 mb-2' />
                       {text}
-                    </div>
+                    </motion.div>
                   ))}
               </div>
 
@@ -83,11 +122,15 @@ const CarDetails = () => {
               </ul>
               </div>
 
-            </div>
-          </div>
+            </motion.div>
+          </motion.div>
 
           {/* right : booking form */}
-          <form onSubmit={handleSubmit} className='shadow-lg h-max sticky top-18 rounded-xl p-6 space-y-6 text-gray-500'>
+          <motion.form 
+          initial={{opacity:0, y:30}}
+          animate={{opacity:1, y:0}}
+          transition={{duration:0.6, delay:0.3}}
+          onSubmit={handleSubmit} className='shadow-lg h-max sticky top-18 rounded-xl p-6 space-y-6 text-gray-500'>
 
             <p className='flex items-center justify-between text-2xl text-gray-800 font-semibold'>{currency}{car.pricePerDay} <span className='text-base text-gray-400 font-normal'>per day</span> </p>
 
@@ -95,14 +138,15 @@ const CarDetails = () => {
 
             <div className='flex flex-col gap-2'>
               <label htmlFor="pickup-date">Pickup Date</label>
-              <input type="date" className='border border-borderColor px-3 py-2 rounded-lg' required id='pickup-date'
-              onChange={(e)=>setPickupDate(e.target.value)}
+              <input value={pickupDate} onChange={(e)=>setPickupDate(e.target.value)}
+               type="date" className='border border-borderColor px-3 py-2 rounded-lg' required id='pickup-date'
               min={new Date().toISOString().split('T')[0]}/>
             </div>
 
             <div className='flex flex-col gap-2'>
               <label htmlFor="return-date">Return Date</label>
-              <input type="date" className='border border-borderColor px-3 py-2 rounded-lg' required id='return-date'
+              <input value={returnDate} onChange={(e)=>setReturnDate(e.target.value)}
+              type="date" className='border border-borderColor px-3 py-2 rounded-lg' required id='return-date'
                min={pickupDate}
               />
             </div>
@@ -112,7 +156,7 @@ const CarDetails = () => {
             </button>
             <p className='text-center text-sm'>No credit card required to reserve</p>
 
-          </form>
+          </motion.form>
 
         </div>
 
