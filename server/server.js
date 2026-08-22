aa5e4e3196d541d69240758b81220f7c2662fc04
Nikhil Chaturvedi <chaturvedinikhil077@gmail.com>
2026-08-22 16:12:57 +0530

    Fixed CORS issue

--- server/server.js
+++ server/server.js
@@ -13,7 +13,10 @@ const app = express();
 await connectDB();
 
 // Middleware 
-app.use(cors());
+app.use(cors({
+  origin : process.env.CLIENT_URL,
+  credentials : true
+}));
 app.use(express.json());
 
 app.get('/', (req,res)=>res.send("Server is running"));
